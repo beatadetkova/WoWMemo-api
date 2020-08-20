edbb1bbf080a26e2c571e779958c9d82b9217588
Beata Detkova <beata.detkova.jr@gmail.com>
2020-08-20 19:41:36 +0200

    Ported code from old repo

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+let db
+if (process.env.NODE_ENV === 'dev') {
+  require('dotenv').config();
+  db = knex({
+    client: 'pg',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USERNAME,
+      password : process.env.DB_PWD,
+      database : process.env.DB_NAME
+    }
+  });
+} else {
+  db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
+}
+
+// const register = require('./controllers/register');
+// const signin = require('./controllers/signin');
+// const profile = require('./controllers/profile');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+// app.get('/', (req, res) => { res.send('it is working!') })
+// app.post('/signin', signin.handleSignin(db, bcrypt))
+// app.post('/register', register.handleRegister(db, bcrypt))
+// app.get('/profile/:id', profile.handleProfileGet(db))
+// app.put('/image', image.handleImage(db))
+// app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT || 3000}`)
+})

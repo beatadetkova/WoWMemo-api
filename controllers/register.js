7051d69381683d5e6ba79ae717fe2fe8178dfa57
Beata Detkova <beata.detkova.jr@gmail.com>
2020-09-10 18:06:28 +0200

    Fixed registration

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,9 @@
+const { sign } = require('../helpers/jwt-utilities.js')
+
 const handleRegister = (db, bcrypt) => async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password ) {
-    return res. status(400).json('incorrect form submission');
+    return res.status(400).json('incorrect form submission');
   }
   const hash = await bcrypt.hash(password, 10);
   db.transaction(trx => {
@@ -13,11 +15,16 @@ const handleRegister = (db, bcrypt) => async (req, res) => {
     .into('users')
     .returning('email')
     .then(email => {
-      const payload = { email }
+      const payload = { email: email[0] }
       const jwt = sign(payload)
       res.json(JSON.stringify({ jwt }))
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .then(trx.commit)
+    .catch(err => {
+      console.log(err)
+      trx.rollback(err)
+      res.status(400).json('unable to register')
+    })
   })
 }
 
